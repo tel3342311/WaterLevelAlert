931429d0186ea8a8607ddd3b7176b62271c827ce
Allen Sun <tel3342311@gmail.com>
2017-02-13 18:35:54 +0800

    Setting, login GUI implement

--- src/com/liteon/waterlevelalert/LoginActivity.java
+++ src/com/liteon/waterlevelalert/LoginActivity.java
@@ -2,12 +2,55 @@ package com.liteon.waterlevelalert;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+	private EditText mUserName;
+	private EditText mPassword;
+	private CheckBox mKeepLogin;
+	private Button mLogin;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		findView();
+		setListener();
+	}
+	
+	private void findView() {
+		mUserName = (EditText) findViewById(R.id.user_name);
+		mPassword = (EditText) findViewById(R.id.password);
+		mKeepLogin = (CheckBox) findViewById(R.id.keep_login);
+		mLogin = (Button) findViewById(R.id.login);
+	}
+	
+	private void setListener() {
+		mKeepLogin.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				
+			}
+			
+		});
+		
+		mLogin.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				
+				setResult(RESULT_OK);
+				finish();
+			}
+			
+		});
 	}
 }
--- src/com/liteon/waterlevelalert/SettingActivity.java
+++ src/com/liteon/waterlevelalert/SettingActivity.java
@@ -1,13 +1,62 @@
 package com.liteon.waterlevelalert;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class SettingActivity extends Activity {
 
+	private static final int REQUEST_CODE = 100;
+	private ToggleButton mLoginToggle;
+	private Button mSkip;
+	private TextView mLoginStatus;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		findView();
+		setListener();
 	}
+	private void findView() {
+		mLoginToggle = (ToggleButton) findViewById(R.id.toggle_login);
+		mLoginStatus = (TextView) findViewById(R.id.login_status);
+		mSkip = (Button) findViewById(R.id.skip);
+	}
+	
+	private void setListener() {
+		mLoginToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+					startActivityForResult(intent, REQUEST_CODE);
+				}
+			}
+		});
+		
+		mSkip.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+        	mLoginToggle.setChecked(true);
+        	mLoginStatus.setText("You are logged in.");
+        } else {
+        	mLoginToggle.setChecked(false);
+        	mLoginStatus.setText("You are not logged in.");
+        }
+    }
 }
