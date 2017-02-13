2ed91e7d95d5d6e59c7c8c0900034f182ebd7400
Allen Sun <tel3342311@gmail.com>
2017-02-13 10:39:28 +0800

    Main activity GUI Implement

--- src/com/liteon/waterlevelalert/MainActivity.java
+++ src/com/liteon/waterlevelalert/MainActivity.java
@@ -1,16 +1,27 @@
 package com.liteon.waterlevelalert;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+	
+	private ImageView mMainView;
+	private ImageView mWaterAlertView;
+	private ImageView mVideoView;
+	private ImageView mDataRecordView;
+	private ImageView mSettingView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViews();
+		setListener();
 	}
 
 	@Override
@@ -27,4 +38,36 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void findViews() {
+		mWaterAlertView = (ImageView) findViewById(R.id.alert_btn);
+		mVideoView = (ImageView) findViewById(R.id.video_btn);
+		mDataRecordView = (ImageView) findViewById(R.id.data_btn);
+		mSettingView = (ImageView) findViewById(R.id.setting_btn);
+	}
+	private void setListener() {
+		mWaterAlertView.setOnClickListener(mOnClickListener);
+		mVideoView.setOnClickListener(mOnClickListener);
+		mDataRecordView.setOnClickListener(mOnClickListener);
+		mSettingView.setOnClickListener(mOnClickListener);
+	}
+	private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Intent intent = null;
+			if (v.getId() == R.id.alert_btn) {
+				intent = new Intent(MainActivity.this, WaterAlertActivity.class);
+			} else if (v.getId() == R.id.video_btn) {
+				intent = new Intent(MainActivity.this, VideoActivity.class);
+			} else if (v.getId() == R.id.data_btn) {
+				intent = new Intent(MainActivity.this, DataRecordActivity.class);
+			} else if (v.getId() == R.id.setting_btn) {
+				intent = new Intent(MainActivity.this, SettingActivity.class);
+			} else {
+				
+			}
+			startActivity(intent);
+		}
+	};
 }
