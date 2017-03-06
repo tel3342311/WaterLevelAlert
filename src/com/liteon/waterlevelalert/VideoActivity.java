3236b90e6f8ac0ab75500cc76ddc1d578c8ecd49
Allen Sun <tel3342311@gmail.com>
2017-03-06 16:37:15 +0800

    Update video page

--- src/com/liteon/waterlevelalert/VideoActivity.java
+++ src/com/liteon/waterlevelalert/VideoActivity.java
@@ -5,6 +5,7 @@ import java.util.TimerTask;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
@@ -13,6 +14,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -30,6 +33,7 @@ public class VideoActivity extends Activity{
 	private int mDuration;
 	private Timer mUpdateUITimer;
 	private boolean isComplete;
+	private LinearLayout mRootLayout;
 	
 	private void startUITimer() {
 		mUpdateUITimer = new Timer();
@@ -61,6 +65,7 @@ public class VideoActivity extends Activity{
 	}
 	
 	private void findView() {
+		mRootLayout = (LinearLayout) findViewById(R.id.video_bg);
 		mVideoView = (VideoView) findViewById(R.id.video_view);
 		mPlay = (ImageView) findViewById(R.id.play);
 		mVolume = (ImageView) findViewById(R.id.volume);
@@ -132,7 +137,7 @@ public class VideoActivity extends Activity{
 	private View.OnClickListener mOnFullScreeenClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-
+			makeVideoFullScreen();
 		}
 	}; 
 	
@@ -151,5 +156,45 @@ public class VideoActivity extends Activity{
 		int current = mVideoView.getCurrentPosition();
 		int progress = current * 100 / mDuration;
 		mProgress.setProgress(progress);
+	}
+	
+	private LinearLayout.LayoutParams defaultVideoViewParams;
+	private int defaultScreenOrientationMode;
+
+	// play video in fullscreen mode
+	private void makeVideoFullScreen() {
+
+	    defaultScreenOrientationMode = getResources().getConfiguration().orientation;
+	    defaultVideoViewParams = (LayoutParams) mVideoView.getLayoutParams();
+
+	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+ 
+    	mVideoView.postDelayed(new Runnable() {
+
+	        @Override
+	        public void run() {
+	        	LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) new LinearLayout.LayoutParams(
+	        			LinearLayout.LayoutParams.MATCH_PARENT,
+	        			LinearLayout.LayoutParams.MATCH_PARENT);
+	            mVideoView.setLayoutParams(params);
+	            mVideoView.layout(10, 10, 10, 10);
+	        }
+	    }, 700);
+	}
+
+
+	// close fullscreen mode
+	private void exitVideoFullScreen() {
+	    setRequestedOrientation(defaultScreenOrientationMode);
+	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+
+	    mVideoView.postDelayed(new Runnable() {
+
+	        @Override
+	        public void run() {
+	        	mVideoView.setLayoutParams(defaultVideoViewParams);
+	        	mVideoView.layout(10, 10, 10, 10);
+	        }
+	    }, 700);
 	}
 }
